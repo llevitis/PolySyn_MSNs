7e8961abbc1e33965f9b4185d95177407508facf
Liza Levitis <elizabeth.levitis@mail.mcgill.ca>
2020-07-31 12:24:02 -0400

    updated relative path to files

--- Code/PolySyn_MSfeature_heatmap.R
+++ Code/PolySyn_MSfeature_heatmap.R
@@ -2,52 +2,52 @@
 
 # Housekeeping
 
-setwd("~/Desktop/NatComms_revisions/")
+setwd("~/code/PolySyn_MSNs/")
 
 library(ggplot2)
 library(reshape2)
 
 # Load in single feature contrast maps (not necessary but good to store with these other data)
 
-SCAx_morph_nodes_effects_lh <- data.frame(CT=read.table("SCAx_CT_DXvNV.txt")[1:152,],
-                                           GM=read.table("SCAx_GM_DXvNV.txt")[1:152,],
-                                           SA=read.table("SCAx_SA_DXvNV.txt")[1:152,],
-                                           MC=read.table("SCAx_MC_DXvNV.txt")[1:152,],
-                                           IC=read.table("SCAx_IC_DXvNV.txt")[1:152,])
-SCAy_morph_nodes_effects_lh <- data.frame(CT=read.table("SCAy_CT_DXvNV.txt")[1:152,],
-                                           GM=read.table("SCAy_GM_DXvNV.txt")[1:152,],
-                                           SA=read.table("SCAy_SA_DXvNV.txt")[1:152,],
-                                           MC=read.table("SCAy_MC_DXvNV.txt")[1:152,],
-                                           IC=read.table("SCAy_IC_DXvNV.txt")[1:152,])
-Downs_morph_nodes_effects_lh <- data.frame(CT=read.table("Downs_CT_DXvNV.txt")[1:152,],
-                                           GM=read.table("Downs_GM_DXvNV.txt")[1:152,],
-                                           SA=read.table("Downs_SA_DXvNV.txt")[1:152,],
-                                           MC=read.table("Downs_MC_DXvNV.txt")[1:152,],
-                                           IC=read.table("Downs_IC_DXvNV.txt")[1:152,])
-Turner_morph_nodes_effects_lh <- data.frame(CT=read.table("Turner_CT_DXvNV.txt")[1:152,],
-                                           GM=read.table("Turner_GM_DXvNV.txt")[1:152,],
-                                           SA=read.table("Turner_SA_DXvNV.txt")[1:152,],
-                                           MC=read.table("Turner_MC_DXvNV.txt")[1:152,],
-                                           IC=read.table("Turner_IC_DXvNV.txt")[1:152,])
-VELOC_morph_nodes_effects_lh <- data.frame(CT=read.table("VELOC_CT_DXvNV.txt")[1:152,],
-                                           GM=read.table("VELOC_GM_DXvNV.txt")[1:152,],
-                                           SA=read.table("VELOC_SA_DXvNV.txt")[1:152,],
-                                           MC=read.table("VELOC_MC_DXvNV.txt")[1:152,],
-                                           IC=read.table("VELOC_IC_DXvNV.txt")[1:152,])
-WAGR_morph_nodes_effects_lh <- data.frame(CT=read.table("WAGR_CT_DXvNV.txt")[1:152,],
-                                           GM=read.table("WAGR_GM_DXvNV.txt")[1:152,],
-                                           SA=read.table("WAGR_SA_DXvNV.txt")[1:152,],
-                                           MC=read.table("WAGR_MC_DXvNV.txt")[1:152,],
-                                           IC=read.table("WAGR_IC_DXvNV.txt")[1:152,])
+SCAx_morph_nodes_effects_lh <- data.frame(CT=read.table("Data/CNVs/SCAx_CT_DXvNV.txt")[1:152,],
+                                           GM=read.table("Data/CNVs/SCAx_CT_DXvNV.txt")[1:152,],
+                                           SA=read.table("Data/CNVs/SCAx_CT_DXvNV.txt")[1:152,],
+                                           MC=read.table("Data/CNVs/SCAx_CT_DXvNV.txt")[1:152,],
+                                           IC=read.table("Data/CNVs/SCAx_CT_DXvNV.txt")[1:152,])
+SCAy_morph_nodes_effects_lh <- data.frame(CT=read.table("Data/CNVs/SCAy_CT_DXvNV.txt")[1:152,],
+                                           GM=read.table("Data/CNVs/SCAy_GM_DXvNV.txt")[1:152,],
+                                           SA=read.table("Data/CNVs/SCAy_SA_DXvNV.txt")[1:152,],
+                                           MC=read.table("Data/CNVs/SCAy_MC_DXvNV.txt")[1:152,],
+                                           IC=read.table("Data/CNVs/SCAy_IC_DXvNV.txt")[1:152,])
+Downs_morph_nodes_effects_lh <- data.frame(CT=read.table("Data/CNVs/Downs_CT_DXvNV.txt")[1:152,],
+                                           GM=read.table("Data/CNVs/Downs_GM_DXvNV.txt")[1:152,],
+                                           SA=read.table("Data/CNVs/Downs_SA_DXvNV.txt")[1:152,],
+                                           MC=read.table("Data/CNVs/Downs_MC_DXvNV.txt")[1:152,],
+                                           IC=read.table("Data/CNVs/Downs_IC_DXvNV.txt")[1:152,])
+Turner_morph_nodes_effects_lh <- data.frame(CT=read.table("Data/CNVs/Turner_CT_DXvNV.txt")[1:152,],
+                                           GM=read.table("Data/CNVs/Turner_GM_DXvNV.txt")[1:152,],
+                                           SA=read.table("Data/CNVs/Turner_SA_DXvNV.txt")[1:152,],
+                                           MC=read.table("Data/CNVs/Turner_MC_DXvNV.txt")[1:152,],
+                                           IC=read.table("Data/CNVs/Turner_IC_DXvNV.txt")[1:152,])
+VELOC_morph_nodes_effects_lh <- data.frame(CT=read.table("Data/CNVs/VELOC_CT_DXvNV.txt")[1:152,],
+                                           GM=read.table("Data/CNVs/VELOC_GM_DXvNV.txt")[1:152,],
+                                           SA=read.table("Data/CNVs/VELOC_SA_DXvNV.txt")[1:152,],
+                                           MC=read.table("Data/CNVs/VELOC_MC_DXvNV.txt")[1:152,],
+                                           IC=read.table("Data/CNVs/VELOC_IC_DXvNV.txt")[1:152,])
+WAGR_morph_nodes_effects_lh <- data.frame(CT=read.table("Data/CNVs/WAGR_CT_DXvNV.txt")[1:152,],
+                                           GM=read.table("Data/CNVs/WAGR_GM_DXvNV.txt")[1:152,],
+                                           SA=read.table("Data/CNVs/WAGR_SA_DXvNV.txt")[1:152,],
+                                           MC=read.table("Data/CNVs/WAGR_MC_DXvNV.txt")[1:152,],
+                                           IC=read.table("Data/CNVs/WAGR_IC_DXvNV.txt")[1:152,])
 
 # read in tables if leave-one-feature-out (LOFO) MS change maps
 
-SCAx_LOFO <- read.table("SCAx_MSNs_LOFO.txt")
-SCAy_LOFO <- read.table("SCAy_MSNs_LOFO.txt")
-Downs_LOFO <- read.table("Downs_MSNs_LOFO.txt")
-Turner_LOFO <- read.table("Turner_MSNs_LOFO.txt")
-VELOC_LOFO <- read.table("VELOC_MSNs_LOFO.txt")
-WAGR_LOFO <- read.table("WAGR_MSNs_LOFO.txt")
+SCAx_LOFO <- read.table("Data/CNVs/SCAx_MSNs_LOFO.txt")
+SCAy_LOFO <- read.table("Data/CNVs/SCAy_MSNs_LOFO.txt")
+Downs_LOFO <- read.table("Data/CNVs/Downs_MSNs_LOFO.txt")
+Turner_LOFO <- read.table("Data/CNVs/Turner_MSNs_LOFO.txt")
+VELOC_LOFO <- read.table("Data/CNVs/VELOC_MSNs_LOFO.txt")
+WAGR_LOFO <- read.table("Data/CNVs/WAGR_MSNs_LOFO.txt")
 
 # make heatmap of correlations between LOFO maps and empirical MS change map
 
